71b2ebb8e22c5afc28953bd2a057527b7620d155
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-04-18 12:03:35 +0200

    zadanie 3.1

--- crudapi/routes/web.php
+++ crudapi/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
