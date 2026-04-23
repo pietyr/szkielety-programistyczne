0077ef1f3af2dcd4e2404978c72368129cf29cfc
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-04-23 15:12:24 +0200

    zadanie-3-2 do dodania AuthController

---
+++ lab-1/zadanie-3-2/laravel-api/app/Http/Controllers/PostController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+        $post = Post::create($fields);
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return $post;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        $fields = $request->validate([
+            'title' => 'string|max:255',
+            'body' => 'string',
+        ]);
+        $post->update($fields);
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json(['message' => 'Post deleted']);
+    }
+}
---
+++ lab-1/zadanie-3-2/laravel-api/app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\PostFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<PostFactory> */
+    use HasFactory;
+
+    protected $fillable = ['title', 'body'];
+}
