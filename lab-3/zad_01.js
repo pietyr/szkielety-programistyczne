444806a89b707778c711d225a104b77ea4f2f156
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-05-15 15:22:50 +0200

    lab3: zadanie 1.9 - route z parametrem

--- lab-3/zad_01.js
+++ lab-3/zad_01.js
@@ -10,6 +10,17 @@ app.get("/about", (req, res) => {
   res.send("Autor strony: Piotr Kisiel");
 });
 
+app.get("/name/:imie", (req, res) => {
+  const imie = req.params.imie;
+  res.status(200).type("text/html").send(`Cześć ${imie}`);
+});
+
+app.get("/name/:imie/:drugie_imie", (req, res) => {
+  const imie = req.params.imie;
+  const drugie_imie = req.params.drugie_imie;
+  res.status(200).type("text/html").send(`Cześć ${imie} i ${drugie_imie}`);
+});
+
 app.listen(port, () => {
   console.log(`Serwer działa na porcie: ${port}`);
 });
