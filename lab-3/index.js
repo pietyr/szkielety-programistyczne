3a95bbb4db11904fcf5b642c5f2538126cc49946
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-05-15 18:01:07 +0200

    lab3: zadanie 1.12 - api

--- lab-3/index.js
+++ lab-3/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const path = require('path')
 const { check, validationResult } = require('express-validator')
+const users = require('./users')
 const app = express()
 const PORT = 3000
 
 // Parser dla danych z formularzy POST
 app.use(express.urlencoded({ extended: false }))
+
+// Parser dla żądania JSON
+app.use(express.json());
 
 // Zadanie 1.10
 
@@ -114,5 +118,64 @@ app.post("/form", [
         <p>Wiek: ${wiek}</p>
     `)
 })
+
+// Zadanie 1.12
+
+app.get('/api/users', (req, res) => {
+    res.json(users)
+})
+
+app.get('/api/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+    if (found) {
+        res.json(users.filter(user => user.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ msg: `Użytkownik o id ${req.params.id} nie został odnaleziony` })
+    }
+})
+
+app.post('/api/users', (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        status: "aktywny"
+    }
+    if (!newUser.name || !newUser.email) {
+        return res.status(400).json({ msg: 'Wprowadź poprawne imię i nazwisko oraz email!' })
+    }
+    users.push(newUser)
+    res.json(users)
+})
+
+app.patch('/api/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+    if (found) {
+        const updUser = req.body
+        users.forEach(user => {
+            if (user.id === parseInt(req.params.id)) {
+                user.name = updUser.name ? updUser.name : user.name
+                user.email = updUser.email ? updUser.email : user.email
+                user.status = updUser.status ? updUser.status : user.status
+                res.json({ msg: 'Dane użytkownika zaktualizowane', user })
+            }
+        })
+    } else {
+        res.status(400).json({ msg: `Użytkownik o id ${req.params.id} nie istnieje!` })
+    }
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+    if (found) {
+        const deletedUser = users.filter(user => user.id === parseInt(req.params.id))
+        // Filtruj tablicę, usuwając użytkownika z danym ID
+        users.splice(users.findIndex(user => user.id === parseInt(req.params.id)), 1)
+        res.json({ msg: 'Użytkownik usunięty', user: deletedUser })
+    } else {
+        res.status(400).json({ msg: `Użytkownik o id ${req.params.id} nie istnieje!` })
+    }
+})
+
 
 app.listen(PORT, ()=> console.log(`Serwer działa na porcie ${PORT}`))
