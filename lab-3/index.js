b17a338b43bf2be185d91ab8cd10085b94f61e9a
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-05-15 17:47:11 +0200

    lab3: zadanie 1.11 - walidacja formularza

--- lab-3/index.js
+++ lab-3/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const { check, validationResult } = require('express-validator')
 const app = express()
 const PORT = 3000
 
 // Parser dla danych z formularzy POST
 app.use(express.urlencoded({ extended: false }))
+
+// Zadanie 1.10
 
 app.get("/form", (req, res) => {
     res.sendFile(path.join(__dirname, "form.html"))
@@ -46,6 +49,69 @@ app.post("/form2-result", (req, res) => {
         <ul>
             ${languagesList}
         </ul>
+    `)
+})
+
+// Zadanie 1.11
+
+app.get("/validation-form", (req, res) => {
+    res.sendFile(path.join(__dirname, "form_validation.html"))
+})
+
+// Własna metoda oczyszczająca
+const createInitials = value => {
+    return value
+        .trim()
+        .split(/\s+/)
+        .map(word => word.charAt(0).toUpperCase())
+        .join('')
+}
+
+app.post("/form", [
+    check('nazwisko')
+        .isLength({ min: 3, max: 25 })
+        .withMessage('Nazwisko musi mieć od 3 do 25 znaków')
+        .isAlpha('pl-PL')
+        .withMessage('Nazwisko może zawierać tylko litery')
+        .trim()
+        .stripLow(),
+
+    check('email')
+        .isEmail()
+        .withMessage('Email jest niepoprawny')
+        .trim()
+        .normalizeEmail()
+        .bail(),
+
+    check('wiek')
+        .isNumeric()
+        .withMessage('Wiek musi być liczbą')
+        .custom(value => {
+            const age = parseInt(value)
+            if (age < 0 || age > 110) {
+                throw new Error('Wiek musi być w zakresie 0-110 lat')
+            }
+            return true
+        })
+        .trim()
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() })
+    }
+
+    let nazwisko = req.body.nazwisko
+    const email = req.body.email
+    const wiek = req.body.wiek
+
+    const initials = createInitials(nazwisko)
+
+    res.send(`
+        <h2>Dane użytkownika:</h2>
+        <p>Nazwisko: ${nazwisko}</p>
+        <p>Inicjały: ${initials}</p>
+        <p>Email: ${email}</p>
+        <p>Wiek: ${wiek}</p>
     `)
 })
 
